706571b52e2459f27c29c80b2474221ba69f347a
danielabertranou-eb <daniela.bertranou@eventbrite.com>
2019-08-30 16:33:38 -0300

    fixed url

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from app.views import Login, Logout
+from app.views import Login, Logout, EventList
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'accounts/logout/$', Logout.as_view(), name='logout'),
     url(r'accounts/login/$', Login.as_view(), name='login'),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', EventList.as_view(), name='event-list'),
     url('', include('social_django.urls', namespace='social')),
 ]
