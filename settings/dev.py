8a1ba538b61a480988fd2917e16e636b5bc90a32
danielabertranou-eb <daniela.bertranou@eventbrite.com>
2019-09-04 12:56:41 -0300

    set new settings config

---
+++ settings/dev.py
@@ -0,0 +1,8 @@
+from .base import *
+from .utils import get_env_variable
+
+
+SOCIAL_AUTH_EVENTBRITE_KEY = get_env_variable('SOCIAL_AUTH_EVENTBRITE_KEY')
+SOCIAL_AUTH_EVENTBRITE_SECRET = get_env_variable(
+    'SOCIAL_AUTH_EVENTBRITE_SECRET',
+)
