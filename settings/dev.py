c1109753987374880d387f25baa1d6d8e21dc5c8
danielabertranou-eb <daniela.bertranou@eventbrite.com>
2019-09-04 17:06:06 -0300

    changed settings config

---
+++ settings/dev.py
@@ -0,0 +1,15 @@
+from .base import *
+from .utils import get_env_variable
+
+
+SOCIAL_AUTH_EVENTBRITE_KEY = get_env_variable('SOCIAL_AUTH_EVENTBRITE_KEY')
+SOCIAL_AUTH_EVENTBRITE_SECRET = get_env_variable(
+    'SOCIAL_AUTH_EVENTBRITE_SECRET',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
